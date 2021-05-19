f40b86459aa3ddd0087a4df367f13897e691cc4d
jimin3263 <dkdldsla0000@naver.com>
2021-05-19 15:43:19 +0900

    fix: sign up and add to person list

--- app/src/main/java/com/example/recycler_kakaotalk/SignUpActivity.java
+++ app/src/main/java/com/example/recycler_kakaotalk/SignUpActivity.java
@@ -14,11 +14,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.recycler_kakaotalk.data.MainData;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -51,17 +53,27 @@ public class SignUpActivity extends AppCompatActivity {
         String email = ((EditText) findViewById(R.id.Email)).getText().toString();
         String password = ((EditText) findViewById(R.id.Password)).getText().toString();
         String CheckPassword = ((EditText) findViewById(R.id.CheckPassword)).getText().toString();
+        String name = ((EditText) findViewById(R.id.Name)).getText().toString();
 
-        if (email.length() > 0 && password.length()>0 && CheckPassword.length()>0 ) {
+        if (email.length() > 0 && password.length()>0 && CheckPassword.length()>0 && name.length()>0 ) {
             if (password.equals(CheckPassword)) {
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    //Log.d(TAG, "createUserWithEmail:success");
 
+                                    //회원 -> 친구목록에 올리도록
+                                    MainData mainData = new MainData();
+                                    mainData.setIv_name(name);
+                                    mainData.setIv_profile("https://firebasestorage.googleapis.com/v0/b/talk-8aece.appspot.com/o/pngegg.png?alt=media&token=05e37d54-f16b-4fbb-8955-eb27a209c5e6");
+
+                                    String uid = task.getResult().getUser().getUid();
+                                    FirebaseDatabase.getInstance().getReference().child("Person").child(uid).setValue(mainData);
+
+                                    //회원가입 성공 -> 로그인 페이지로 이동
                                     Intent intent= new Intent(SignUpActivity.this,LoginActivity.class);
                                     startActivity(intent);
                                 } else {
